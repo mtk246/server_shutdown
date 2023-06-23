1e3a86c12ee0fcf2340005a0d8fa3909cdd2fa11
Ngwe Sin Sandar <ngwesinsandar112@gmail.com>
2023-06-23 11:03:24 +0630

    interview solution

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head';
 import { getApi } from '@/utils/api';
-import { useEffect, useCallback } from 'react';
+import { useEffect, useCallback, useState } from 'react';
 
 export default function Home() {
+  const [obj, setObj] = useState({});
 
   const getProductDetails = useCallback(async () => {
-    const fetchApi = getApi(process.env.API_URL);
+    const fetchApi = getApi("https://test-api.takeitnow.io/api/product");
 
     await fetchApi.then((res) => {
       console.log(res.data);
+      setObj(res.data.obj[0]);
     });
   }, []);
 
@@ -21,7 +23,19 @@ export default function Home() {
       <Head>
         <title>NextJS Interview</title>
       </Head>
-      <div className="text-danger">hello</div>
+      <div className='row'>
+        {
+          Object.keys(obj).map(key => {
+            return (
+              <div key={key} className='col-md-4 col-lg-2 mb-2'>
+                <h1>{key}</h1>
+                <h4>Min - {obj[key].min}</h4>
+                <h4>Max - {obj[key].max}</h4>
+              </div>
+            )
+          })
+        }
+      </div>
     </>
   )
 }
